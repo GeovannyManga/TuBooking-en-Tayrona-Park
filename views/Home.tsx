2ed9b84b092d72ecc5793c7e7d9e4cd9ff288a78
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2025-07-08 01:28:20 -0500

    import dinamico

--- views/Home.tsx
+++ views/Home.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import dynamic from 'next/dynamic';
 import NavBar from "../components/NavBar";
-import Banner from "../components/Banner";
 import CardRegion from "../components/CardRegion";
 import CarrucelRegion from "../components/CarrucelRegion";
 import Cards from "../components/Cards";
 import Footer from "../components/Footer";
 import Legal from "../components/legal";
 
+// 🔁 Carga dinámica del componente con SSR desactivado
+const Banner = dynamic(() => import("../components/Banner"), { ssr: false });
+
 export default function Home() {
   return (
-    <div className=''>
-         <NavBar></NavBar>
-         <Banner></Banner>
-         <CardRegion></CardRegion>
-         <Cards></Cards>
-         <Legal></Legal>
-         <CarrucelRegion></CarrucelRegion>
-         <Footer></Footer>
-
+    <div>
+      <NavBar />
+      <Banner />
+      <CardRegion />
+      <Cards />
+      <Legal />
+      <CarrucelRegion />
+      <Footer />
     </div>
-  )
+  );
 }
