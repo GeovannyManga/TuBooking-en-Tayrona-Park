8de18ab7da326987bc6af15f1d8f53eb4cccf313
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2025-07-08 01:13:30 -0500

    intento de mejora al cargar

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,20 @@
 'use client';
 
+import { useState } from 'react';
+
 export default function Banner() {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <div className="relative w-full h-[85vh] overflow-hidden">
-      {/* Video de YouTube como fondo adaptado sin bordes */}
+    <div className="relative w-full h-[85vh] overflow-hidden bg-black">
+      {/* Loader mientras se carga el video */}
+      {loading && (
+        <div className="absolute inset-0 z-20 flex items-center justify-center bg-black">
+          <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      )}
+
+      {/* Video de fondo de YouTube */}
       <div className="absolute inset-0 overflow-hidden pointer-events-none">
         <iframe
           className="absolute top-1/2 left-1/2 w-[350vw] lg:w-full h-[300vh] -translate-x-1/2 -translate-y-1/2"
@@ -12,13 +23,14 @@ export default function Banner() {
           frameBorder="0"
           allow="autoplay; encrypted-media"
           allowFullScreen
+          onLoad={() => setLoading(false)} // Oculta loader al cargar
         ></iframe>
       </div>
 
       {/* Capa de oscurecimiento suave */}
       <div className="absolute inset-0 bg-black/20 z-0 pointer-events-none" />
 
-      {/* Capa de texto encima */}
+      {/* Texto encima del video */}
       <div className="absolute inset-0 flex flex-col items-center justify-center z-10 text-center px-4 pointer-events-none">
         <h1 className="text-white text-xl sm:text-2xl lg:text-6xl font-bold">
           Bienvenido a
