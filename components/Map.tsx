71fbc19afa22752a25f866e1045c565088af0485
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2025-12-01 15:45:59 -0500

    cambios en puntos de ubicacion

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useRef, useState } from "react";
 import L from "leaflet";
 import cabana from "../public/icono.png";
+import point from "../public/punto.png";
 import "leaflet/dist/leaflet.css";
 
 
@@ -34,10 +35,89 @@ const Map = () => {
       iconAnchor: [16, 32],
       popupAnchor: [0, -32],
     });
+        const punto = L.icon({
+      iconUrl: point.src,
+      iconSize: [32, 32],
+      iconAnchor: [16, 32],
+      popupAnchor: [0, -32],
+    });
+
 
     // Agregamos el marcador
-    L.marker([11.2899303, -73.9123437], { icon: cabanaIcon })
-      .bindTooltip("Eden Tayrona Park")
+    L.marker([11.281187,-73.914812], { icon: punto })
+      .bindTooltip("Entrada del Zaino")
+      .addTo(initializedMap);
+          // Agregamos el marcador
+    L.marker([11.305063,-73.928813], { icon: punto })
+      .bindTooltip("Parqueadero del parque tayrona")
+      .addTo(initializedMap);
+          // Agregamos el marcador
+    L.marker([11.306188,-73.930563], { icon: punto })
+      .bindTooltip("Punto de partida")
+      .addTo(initializedMap);
+           // Agregamos el marcador
+    L.marker([11.306138,-73.930703], { icon: cabanaIcon })
+      .bindTooltip("Izquierda caballeriza")
+      .addTo(initializedMap);
+          // Agregamos el marcador
+    L.marker([11.306487,-73.930516], { icon: punto })
+      .bindTooltip("Mano derecha sendero cañaveral")
+      .addTo(initializedMap);
+           // Agregamos el marcador
+    L.marker([11.306313,-73.930953], { icon: punto })
+      .bindTooltip("Derecho sendero arrecife y Cabo San Juan")
+      .addTo(initializedMap);
+          // Agregamos el marcador
+    L.marker([11.311937,-73.935688], { icon: punto })
+      .bindTooltip("Piscinita cañaveral")
+      .addTo(initializedMap);
+            // Agregamos el marcador
+    L.marker([11.303688,-73.929812], { icon: cabanaIcon })
+      .bindTooltip("puesto de artesanías y recuerdos mauricio el indígena")
+      .addTo(initializedMap);
+         // Agregamos el marcador
+    L.marker([11.312687,-73.950438], { icon: punto })
+      .bindTooltip("Sector arrecife")
+      .addTo(initializedMap);
+            // Agregamos el marcador
+    L.marker([11.312437,-73.949687], { icon: cabanaIcon })
+      .bindTooltip("Jugos naturales de los indígenas")
+      .addTo(initializedMap);
+           // Agregamos el marcador
+    L.marker([11.314187,-73.950438], { icon: cabanaIcon })
+      .bindTooltip("Panadería bere")
+      .addTo(initializedMap);
+            // Agregamos el marcador
+    L.marker([11.320187,-73.954937], { icon: punto })
+      .bindTooltip("Playa arenilla")
+      .addTo(initializedMap);
+          // Agregamos el marcador
+    L.marker([11.322812,-73.956688], { icon: punto })
+      .bindTooltip("Playa piscina natural")
+      .addTo(initializedMap);
+           // Agregamos el marcador
+    L.marker([11.327813,-73.963688], { icon: cabanaIcon })
+      .bindTooltip("Tayrona subacuático")
+      .addTo(initializedMap);
+           // Agregamos el marcador
+    L.marker([11.329763,-73.964359], { icon: punto })
+      .bindTooltip("Playa Cabo San Juan")
+      .addTo(initializedMap);
+          // Agregamos el marcador
+    L.marker([11.330938,-73.968563], { icon: punto })
+      .bindTooltip("Playa boca del saco (playa nudista)")
+      .addTo(initializedMap);
+         // Agregamos el marcador
+    L.marker([11.317438,-73.980937], { icon: punto })
+      .bindTooltip("Pueblo sagrado chairama")
+      .addTo(initializedMap);
+        // Agregamos el marcador
+    L.marker([11.336813,-73.990437], { icon: punto })
+      .bindTooltip("Playa brava")
+      .addTo(initializedMap);
+         // Agregamos el marcador
+    L.marker([11.287187,-74.001063], { icon: punto })
+      .bindTooltip("Entrada calabazo")
       .addTo(initializedMap);
 
     // Limpiamos el mapa cuando el componente se desmonte
